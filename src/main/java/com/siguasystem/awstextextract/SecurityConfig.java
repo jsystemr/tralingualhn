7485808ae487f9bf7e78d561f406d8a2bb9eebea
jsystemr <jsystemr@gmail.com>
2025-02-22 16:01:33 -0600

    token
    almacena token

--- src/main/java/com/siguasystem/awstextextract/SecurityConfig.java
+++ src/main/java/com/siguasystem/awstextextract/SecurityConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-	@Bean
+	/*@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		XorCsrfTokenRequestAttributeHandler requestHandler = new XorCsrfTokenRequestAttributeHandler();
 		// set the name of the attribute the CsrfToken will be populated on
@@ -22,5 +23,16 @@ public class SecurityConfig {
 				.csrfTokenRequestHandler(requestHandler)
 			);
 		return http.build();
-	}
+	}*/
+	 @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) // Almacena el token en una cookie
+            )
+            .authorizeRequests(auth -> auth
+                .anyRequest().authenticated()
+            );
+        return http.build();
+    }
 }
